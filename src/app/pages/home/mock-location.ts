b584567441ab9fde9e4cd1f7d8b1ec22336ee7c2
Abdul Rehman Raja <abdulrehmanraaja@gmail.com>
2018-11-27 02:01:05 +0500

    Arr of Data For DataBinding

---
+++ src/app/pages/home/mock-location.ts
@@ -0,0 +1,15 @@
+import { Location } from './location' ;
+
+export const Locations : Location[] = [
+
+    { id: 1, name: 'Rawalpindi' },
+    { id: 2, name: 'Islamabad' },
+    { id: 3, name: 'Lahore' },
+    { id: 4, name: 'Karachi' },
+    { id: 5, name: 'Peshawar' },
+    { id: 6, name: 'Quetta' },
+    { id: 7, name: 'Gilgit' },
+    { id: 8, name: 'Hunza' },
+    { id: 9, name: 'Sakardu' },
+    { id: 10, name: 'Chitral' }
+];
