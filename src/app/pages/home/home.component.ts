81621d9ce23346cf935aff34eb2ca05f829845d2
Abdul Rehman Raja <abdulrehmanraaja@gmail.com>
2018-11-27 01:59:53 +0500

    Created Template Drive Form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Location } from './location';
+import { Locations } from './mock-location';
+import { FormsModule} from '@angular/forms';
+import { ClsSearchVehicle} from './cls-search-vehicle' ;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,7 +11,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  locations = Locations;
+  userModel = new ClsSearchVehicle('10/12/2018',2,3);
   ngOnInit() {
   }
 
